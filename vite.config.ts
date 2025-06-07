a0464e1bee85e8a4b14723a3b5908937b7fcca6a
menteora <menteora@gmail.com>
2025-06-07 14:13:27 +0200

    git hub pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig, loadEnv } from 'vite';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      // base: '/react-markdown-blog/',
+      base: '/react-markdown-blog/',
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
